896e7320bc86010330505df9f20436da86656c60
linlizlou <linlizlou@users.noreply.github.com>
2022-04-06 00:32:28 +0000

    deploy: 22201daab89f46f387e72fc4ab55f003b2adf104

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
+var firstName = "Lindsey";
+var lastName = "Hill";
+var age = "21";
+var favoriteFood = "ice cream";
 
+console.log(
+  `My name is ${firstName}. I am ${age} years old and I love to eat ${favoriteFood}.`
+);
+
+/* make a variable called num with a value of 2.
+add 2+2 and store result in a new variable called result.
+console.log result.
+console.log full name as a var called fullName.
+*/
+
+var num = "2";
+var result = num + num;
+
+console.log(result);
+
+var fullName = `${firstName} ${lastName}`;
