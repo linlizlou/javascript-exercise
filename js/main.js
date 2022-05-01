5b10f50816ca76168b40e1b2515dee1a36a47eeb
linlizlou <linlizlou@users.noreply.github.com>
2022-05-01 22:59:30 +0000

    deploy: 20dc0394b1eb9ee8800b80b96db9dc51963d3e68

--- js/main.js
+++ js/main.js
@@ -56,3 +56,32 @@ function next() {
 // Wire it up
 previousButton.addEventListener("click", previous);
 nextButton.addEventListener("click", next);
+
+// get
+
+const btns = document.querySelectorAll("[data-target]");
+const close_modals = document.querySelectorAll(".close-modal");
+const overlay = document.getElementById("overlay");
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    document.querySelector(btn.dataset.target).classList.add("active");
+    overlay.classList.add("active");
+  });
+});
+
+close_modals.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const modal = btn.closest(".modal");
+    modal.classList.remove("active");
+    overlay.classList.remove("active");
+  });
+});
+
+window.onclick = (event) => {
+  if (event.target == overlay) {
+    const modals = document.querySelectorAll(".modal");
+    modals.forEach((modal) => modal.classList.remove("active"));
+    overlay.classList.remove("active");
+  }
+};
