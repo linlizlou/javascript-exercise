035a4b9ff2081a11e124fe750d8d22b723745731
linlizlou <linlizlou@users.noreply.github.com>
2022-05-01 06:23:16 +0000

    deploy: 3c88871431b72db5f721363f6446ec81ce8d67a7

--- js/main.js
+++ js/main.js
@@ -22,3 +22,37 @@ function stickyNav() {
     navbar.classList.remove("sticky");
   }
 }
+
+// Get
+var activeSlideshow = 0;
+var previousButton = document.querySelector("#previous");
+var nextButton = document.querySelector("#next");
+var slides = document.querySelectorAll(".slideshow > div");
+
+// Define
+function toggleSlide(from, to) {
+  slides[from].classList.remove("activeSlideshow");
+  slides[to].classList.add("activeSlideshow");
+}
+
+function previous() {
+  var from = active;
+  if (activeSlideshow <= 0) {
+    activeSlideshow = slides.length - 1;
+  } else {
+    activeSlideshow--;
+  }
+  toggleSlide(from, activeSlideshow);
+}
+function next() {
+  var from = activeSlideshow;
+  if (activeSlideshow >= slides.length - 1) {
+    activeSlideshow = 0;
+  } else {
+    activeSlideshow++;
+  }
+  toggleSlide(from, activeSlideshow);
+}
+// Wire it up
+previousButton.addEventListener("click", previous);
+nextButton.addEventListener("click", next);
